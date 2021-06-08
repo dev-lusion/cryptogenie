851107bb16f89dd2c46325a6dcdadaa5f3222776
Ayush Maurya <ayushmaurya.work@gmail.com>
2021-06-08 19:11:52 +0530

    refactor:title updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,31 +1,42 @@
-import React,{useState} from 'react'
-import logo from './../Vector3.png';
-import 'boxicons'
+import React, { useState } from "react";
+import logo from "./../Vector3.png";
+import "boxicons";
 const Header = () => {
-    const [expand,setExpand]= useState(false);
-    return (
-        <div className='header'>
-            <div className='header__logo'>
-            <h1 > <span>crypto</span>.<span>genie</span></h1>
-            <img src={logo}alt="" />
-            </div>
-            <div className="header__search">
-                <div className={`header__search__in ${expand ?'expand':''}`}>
-                    <div className="header__search__in__icon "> 
-                     <box-icon name='search'color="#dbdbdb"></box-icon></div>
-                    <input type="text" placeholder="Search cryptocurrency..." onClick={()=>setExpand(!expand)}/>
-                </div>
-            </div>
-            <div className="header__user">
-            <div className='header__user__notify'>
-            <box-icon name='bell' color='#dbdbdb' ></box-icon>
-            </div>
-            <div className='header__user__avatar'> 
-            <img src="https://lh3.googleusercontent.com/a-/AOh14Gj-IJeCFLlxdot5OBlWsXYdmFHzLTm4QoQjyp91XQ=s96-c" alt="" />
-            <h3>Ayush Maurya</h3>
-            </div>
-            </div>
+  const [expand, setExpand] = useState(false);
+  return (
+    <div className="header">
+      <div className="header__logo">
+        <h1>
+          {" "}
+          <span>crypto</span>.<span>genie</span>
+        </h1>
+        <img src={logo} alt="" />
+      </div>
+      <div className="header__search">
+        <div className={`header__search__in ${expand ? "expand" : ""}`}>
+          <div className="header__search__in__icon ">
+            <box-icon name="search" color="#dbdbdb"></box-icon>
+          </div>
+          <input
+            type="text"
+            placeholder="Search cryptocurrency..."
+            onClick={() => setExpand(!expand)}
+          />
         </div>
-    )
-}
+      </div>
+      <div className="header__user">
+        <div className="header__user__notify">
+          <box-icon name="bell" color="#dbdbdb"></box-icon>
+        </div>
+        <div className="header__user__avatar">
+          <img
+            src="https://lh3.googleusercontent.com/a-/AOh14Gj-IJeCFLlxdot5OBlWsXYdmFHzLTm4QoQjyp91XQ=s96-c"
+            alt=""
+          />
+          <h3>Ayush Maurya</h3>
+        </div>
+      </div>
+    </div>
+  );
+};
 export default Header;
